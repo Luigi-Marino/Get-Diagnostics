05d48e488a4e1ad6f022e4f8aae5fefc8a8eb35a
Luigi Marino <emails@luigi-marino.com>
2026-05-05 16:27:57 +0100

    Format dates

--- modules/SystemInfo.psm1
+++ modules/SystemInfo.psm1
@@ -3,12 +3,16 @@ function Get-SystemInfo {
     $cs = Get-CimInstance Win32_ComputerSystem
     
     return [ordered]@{
-        Hostname    = $env:COMPUTERNAME
-        User        = $cs.UserName
-        OS          = $os.Caption
-        Version     = $os.Version
-        Model       = $cs.Model
-        LastBoot    = $os.LastBootUpTime
+        Hostname        = $env:COMPUTERNAME
+        User            = $cs.UserName
+        OS              = $os.Caption
+        Version         = $os.Version
+        Manufacturer    = $cs.Manufacturer
+        Model           = $cs.Model
+        SerialNumber    = (Get-CimInstance Win32_BIOS).SerialNumber
+        LastBoot        = [Management.ManagementDateTimeConverter]::ToDateTime($os.LastBootUpTime)
+        InstallDate     = [Management.ManagementDateTimeConverter]::ToDateTime($os.InstallDate)
+
     }
 }
 
